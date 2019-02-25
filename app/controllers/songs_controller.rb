87c5f744253e6b9653dfcb2fa97bf6c3cc00ed9c
Jessica Anderson <jessie.leigh30@gmail.com>
2019-02-25 16:56:11 -0700

    add logout to nav

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,25 +1,41 @@
 class SongsController < ApplicationController
+  # before_action :set_artist
+  before_action :set_chart
   before_action :set_song, only: [:show, :edit, :update, :destroy]
 
   def index
     @songs = @chart.songs.all
+    @songs = @artist.songs.all
   end
 
   def show
   end
   
   def new
-    @song = @chart.songs.new(song_params)
-    if @song.save
-      redirect_to chart_songs_path
+    @song = @chart.songs.new
+    render partial: 'form'
+  end
+
+  def update
+    if @song.update(song_params)
+      redirect_to @chart
+    else 
+      render partial: "form"
     end
   end
 
   private
 
   def set_song
-    @song = Song.find(params[:song_id])
+    @song = Song.find(params[:id])
   end
 
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+
+  def set_chart
+    @chart = Chart.find(params[:chart_id])
+  end
 
 end
